240060c8042f54f05c49319ae6b61365a455c1b6
cathyxzhu <cxzhu@usc.edu>
2023-11-28 07:49:44 -0800

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -98,4 +98,4 @@ client = mqtt.Client()
 # client.on_message = on_message
 client.on_connect = on_connect
 client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
-client.loop_start()
+client.loop_forever()
